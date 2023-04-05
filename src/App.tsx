5e9e72b48fba3851254179d57e4fbcca80d0b72e
Adam Doty <acdoty15@gmail.com>
2023-04-05 09:55:12 -0700

    Add props to GoalTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import GoalTracker from "./components/GoalTracker";
 
+const hike = {
+  name: "Half Dome",
+  distance: 17,
+  difficulty: "Very Hard",
+  location: "Yosemite National Park, CA",
+  climb: 5200,
+};
+
+const time = {
+  date: new Date("2023-08-14"),
+  daysLeft: 7,
+};
+
 function App() {
   return (
     <div>
-      <GoalTracker />
+      <GoalTracker hike={hike} time={time} />
     </div>
   );
 }
--- src/components/GoalTracker.tsx
+++ src/components/GoalTracker.tsx
@@ -7,16 +7,34 @@ import {
   Heading,
   Text,
 } from "@chakra-ui/react";
-const GoalTracker = () => {
+
+interface Props {
+  hike: Hike;
+  time: { date: Date; daysLeft: number };
+}
+
+interface Hike {
+  name: string;
+  distance: number;
+  difficulty: string;
+  location: string;
+  climb: number;
+}
+
+const GoalTracker = ({ hike, time }: Props) => {
   return (
     <Card align="center" width="400px">
       <CardHeader>
-        <Heading size="md">Hiking Goal Title</Heading>
+        <Heading size="md">{hike.name}</Heading>
       </CardHeader>
       <CardBody>
-        <Text>Hiking Goal Distance</Text>
-        <Text>Hiking Goal Challenge Rating</Text>
-        <Text>Hiking Goal Time</Text>
+        <Text>{hike.distance} mi</Text>
+        <Text>{hike.difficulty}</Text>
+        <Text>{hike.location}</Text>
+        <Text>{hike.climb}</Text>
+        <Text>
+          {time.daysLeft} days until {time.date.toDateString()}
+        </Text>
       </CardBody>
       <CardFooter>
         <Button colorScheme="blue">Setup Training Plan</Button>
