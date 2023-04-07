62cde5b1fe5965401fbec6fdf8316f2cd0c7ecaf
Adam Doty <acdoty15@gmail.com>
2023-04-07 15:32:01 -0700

    Updated Circular Progress Bar

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 
 import { Metric } from "./MetricGrid";
+import { MdOutlineCheck } from "react-icons/md";
 
 interface Props {
   metric: Metric;
@@ -30,10 +31,30 @@ const GoalMetricCard = ({ metric, highestValue }: Props) => {
           <Icon as={metric.icon} />
         </HStack>
       </CardHeader>
-      <CardBody display="flex" flexDirection="column" alignItems="center">
-        <CircularProgress value={progress} size="80px" thickness="4px">
-          <CircularProgressLabel>{progress.toFixed(0)}%</CircularProgressLabel>
-        </CircularProgress>
+      <CardBody
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        pt={0}
+      >
+        {progress >= 100 ? (
+          <CircularProgress
+            value={progress}
+            size="80px"
+            thickness="4px"
+            color="green"
+          >
+            <CircularProgressLabel display="flex" justifyContent="center">
+              <Icon as={MdOutlineCheck} boxSize={10} />
+            </CircularProgressLabel>
+          </CircularProgress>
+        ) : (
+          <CircularProgress value={progress} size="80px" thickness="4px">
+            <CircularProgressLabel display="flex" justifyContent="center">
+              {progress.toFixed(0)}%
+            </CircularProgressLabel>
+          </CircularProgress>
+        )}
         {metric.name === "Difficulty" ? (
           <Text>
             {difficultyMap[highestValue]} {">"} {difficultyMap[metric.value]}{" "}
